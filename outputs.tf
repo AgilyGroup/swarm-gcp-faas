083d4d58728c5e3633a49ba424b0d7952959908f
Vincent Caggiari <vincent@edencia.com>
2018-09-28 00:10:37 +0200

    added output

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,7 @@ output "swarm_managers" {
 output "swarm_workers" {
   value = "${concat(google_compute_instance.worker.*.name, google_compute_instance.worker.*.network_interface.0.access_config.0.assigned_nat_ip)}"
 }
+
+output "swarm_network_name" {
+  value = "${google_compute_network.swarm.name}"
+}
